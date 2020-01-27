ecdf2b0a4e08228b16fcc170461bf00114973124
tommyyun96 <40390211+tommyyun96@users.noreply.github.com>
2020-01-27 15:33:28 -0500

    Sidebar practice

---
+++ client/src/MainLayout/index.js
@@ -0,0 +1,23 @@
+import React, { Fragment, Component } from 'react'
+import SideBar from '../SideBar'
+
+class MainLayout extends Component {
+    render() {
+        return (
+            <Fragment>
+                {/*<AppHeader />*/}
+                <div className="app-main">
+                    <SideBar />
+                    <div className="app-main__outer">
+                        <div className="app-main__inner">
+                            Hi, I'm the body
+                        </div>
+                        {/*<AppFooter />*/}
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default MainLayout
---
+++ client/src/SideBar/index.js
@@ -0,0 +1,57 @@
+import React, { Fragment, Component } from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import cx from 'classnames'
+
+
+
+export default class SideBar extends Component {
+    render() {
+        let {
+            backgroundColor,
+            enableBackgroundImage,
+            enableSidebarShadow,
+            backgroundImage,
+            backgroundImageOpacity,
+        } = this.props;
+
+        return (
+            <Fragment>
+                {/*<div className="sidebar-mobile-overlay" onClick={this.toggleMobileSidebar} />*/}
+                <ReactCSSTransitionGroup
+                    component="div"
+                    className={cx("app-sidebar", backgroundColor, { 'sidebar-shadow': enableSidebarShadow })}
+                    transitionName="SidebarAnimation"
+                    transitionAppear={true}
+                    transitionAppearTimeout={1500}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    {/*<HeaderLogo />*/}
+                    <PerfectScrollbar>
+                        <div className="app-sidebar__inner">
+                            {/*<Nav />*/}
+                            <h5 className="app-sidebar__heading">PRO VERSION</h5>
+                            <div className="metismenu vertical-nav-menu">
+                                <ul className="metismenu-container">
+                                    <li className="metismenu-item">
+                                        <a className="metismenu-link" href="https://dashboardpack.com/theme-details/architectui-dashboard-react-pro" target="_blank">
+                                            <i className="metismenu-icon pe-7s-diamond"></i>
+                                            Upgrade to PRO
+                            </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <h5 className="app-sidebar__heading">Menu</h5>
+                        </div>
+                    </PerfectScrollbar>
+                    <div
+                        className={cx("app-sidebar-bg", backgroundImageOpacity)}
+                        style={{
+                            backgroundImage: enableBackgroundImage ? 'url(' + backgroundImage + ')' : null
+                        }}>
+                    </div>
+                </ReactCSSTransitionGroup>
+            </Fragment>
+        )
+    }
+}
